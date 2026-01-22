b04af9af05271a1e83c7cb46a43fa92512b7a1f6
fariswheel <farismahmood17@gmail.com>
2026-01-22 20:12:09 +0400

    Feature B added

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,15 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World"); // Prints the message to the console
+
+        featureB();
     }
+
+    public static void featureB() {
+    System.out.println("Feature B is running");
 }
+
+}
+
+
+
